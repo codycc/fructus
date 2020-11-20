105d32bacf0496f075c2a6ac16ed7b8508199a21
Cody Condon <linkmecody@gmail.com>
2020-11-20 18:27:31 -0500

    create fruit card view and onboarding view

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Cody Condon on 2020-11-20.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+                OnboardingView()
+        }
+    }
+}
